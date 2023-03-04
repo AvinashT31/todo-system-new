032309a6517517a0e304558a86fe216897f1a294
AvinashT31 <59430438+AvinashT31@users.noreply.github.com>
2023-03-04 12:22:07 +0530

    update

--- index.js
+++ index.js
@@ -15,25 +15,36 @@ function AddData(){
 
 function displayTodo(){
 
-    var myInfoFromLS = JSON.parse(localStorage.getItem("myInfoFromLS"));
-    console.log(myInfoFromLS,"myInfoFromLS" );
+  var studentInfo = document.getElementById("displayStudentInfo");
+  console.log(studentInfo, "studentInfo");
+  
+  var myInfoFromLS = JSON.parse(localStorage.getItem("myInfoFromLS"));
+  console.log(myInfoFromLS,"myInfoFromLS" );
 
-    var studentInfo = document.getElementById("displayStudentInfo");
-    console.log(studentInfo, "studentInfo");
+    
+  var student =[`<div id="row"><p>Task</p><p>Completed</p></div>`];
 
-    var student =[`<div id="row"><p>Task</p><p>Completed</p></div>`];
+  for (let i= 0; i < myInfoFromLS.length; i++) {
 
-    for (let i= 0; i < myInfoFromLS.length; i++) {
-
-      student += `<div id="content"><p>${myInfoFromLS[i]}</p>
-      <i class="fa-solid fa-trash"></i>
-      </div>`
+    student += `<div id="content"><p>${myInfoFromLS[i]}</p>
+    <i onclick = "Delete()" class="fa-solid fa-trash"></i>
+    </div>`
         
     }
      console.log(student, "student");
      studentInfo.innerHTML = student;
-   }
+
+}
 displayTodo()
 
 
+function Delete(TodoIndex){
 
+  var DataFromLS = JSON.parse(localStorage.getItem("myInfoFromLS"));
+  DataFromLS.splice(TodoIndex, 1);
+  localStorage.setItem("myInfoFromLS", JSON.stringify(DataFromLS));
+  displayTodo();
+  alert("Delete Successfully")
+
+
+}
